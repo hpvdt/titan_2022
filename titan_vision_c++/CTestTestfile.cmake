f4b848cad7c44a7aaa4afbff73762bc6e43ae0f8
HPVDT Toronto <57300333+hpvdt@users.noreply.github.com>
2022-07-21 18:03:13 -0400

    Delete titan_vision_c++ directory
    Not using this c++ code

--- titan_vision_c++/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/pi/79BD-F729/HPVDT/titan/titan_vision_c++
-# Build directory: /media/pi/79BD-F729/HPVDT/titan/titan_vision_c++
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testBike "bikeExecutables")
--- titan_vision_c++/src/bikePi.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int bikePi();
-int sparePi();
