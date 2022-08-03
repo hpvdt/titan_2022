fd30f51103d997b5a73a9abf902fdbbe55058023
Savo <56771837+savob@users.noreply.github.com>
2022-08-03 19:40:12 -0400

    Added Arduino based telemetry station code

---
+++ Microcontroller Code/Microcontroller Unit Tests/serialPassThroughNano/serialPassThroughNano.ino
@@ -0,0 +1,70 @@
+// FOR ARDUINO NANO
+#include <RF24.h>
+
+const byte radioInteruptPin = 2;
+const byte radioCSPin = 8;
+const byte radioEnablePin = 7;
+
+// Hardware configuration
+RF24 radio(radioEnablePin, radioCSPin);
+bool recieverRole = false; // Are we the reciever? Vehicle is 'reciever', station is 'sender'
+
+const int requestTimeout = 10;
+
+// Communication pipes to use
+const uint64_t pipes[2] = { 0x1122334471LL, 0x112233447CLL };
+
+volatile bool recievedRadioData = false; // Used to record a recieve event
+char radioMessage[32]; // Buffer for message (nRF24 messages are limited to 32 bytes each)
+byte pipeNumber; // Stores the origin of the message (used in reciever mode)
+
+unsigned long baudrate = 115200; // Baudrate on used on all serial lines
+bool debugMode = false;
+
+
+
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Set up the serial lines
+  Serial.begin(baudrate);
+  
+  if (debugMode == true) {
+    // Wait for debugging connection, assumed to be over USB
+    while (!Serial) {
+      delay(100);
+    }
+    Serial.println("DEBUGGING MODE ACTIVE");
+  }
+
+  setupRadio();
+  //blinker(1000, 3);
+
+  if (debugMode == true) {
+    Serial.println("GO TIME");
+  }
+}
+
+void loop() {
+  if (Serial.available()) {
+    processData('h');
+  }
+  if (recievedRadioData) {
+    processData('t');
+    recievedRadioData = false; // Reset flag
+  }
+  delay(10);
+}
+
+void blinker (const int period, const byte repeats) {
+  // Blink LED a number of times for a given length
+  for (byte i = 0; i < repeats; i++) {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(period / 2);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(period / 2);
+  }
+  // Note on STM32 LED is on when pin is LOW!
+}
---
+++ Microcontroller Code/Microcontroller Unit Tests/serialPassThroughNano/telemetry.ino
@@ -0,0 +1,76 @@
+void setupRadio() {
+    // Set up the radio
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);          // Low power, raise if a decoupling capacitor is added
+  radio.setDataRate(RF24_250KBPS);
+  radio.setAutoAck(1);                    // Ensure autoACK is disabled
+  radio.enableAckPayload();               // Allow optional ack payloads
+  radio.setRetries(10, 15);                // Smallest time between retries, max no. of retries
+
+  radio.enableDynamicPayloads();
+  radio.setChannel(100);
+
+  if (recieverRole == true) {
+    // Reciever
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1, pipes[0]);
+    radio.startListening();
+  }
+  else {
+    // Sender
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1, pipes[1]);
+    radio.stopListening();
+  }
+  radio.maskIRQ(1, 1, 0);                 // Only toggles interrupt (falling edge/LOW) when recieving data
+  //radio.printDetails();
+  
+  // Interrupts on recieving a message
+  pinMode(radioInteruptPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(radioInteruptPin), radioRecieved, FALLING); // Set flagging interrupt
+}
+
+void checkRadio () {
+  recievedRadioData = true; // Set flag that something was recieved
+}
+
+void radioRecieved() {
+  recievedRadioData = true;
+  memset(radioMessage, 0, sizeof(radioMessage)); // Clear buffer
+  
+  // Start by getting where the message is from
+  radio.available(&pipeNumber); // Get pipe the message we recieved on in the event a reply is needed
+
+  // Read the message. Reads both messages and acknoledgements. 
+  int messageLen = radio.getDynamicPayloadSize();
+  radio.read(&radioMessage, messageLen);
+
+  if (debugMode) {
+    Serial.print("Recieved on nRF24: '");
+    Serial.print(radioMessage);
+    Serial.print("'. Length of ");
+    Serial.println(messageLen);
+  }
+}
+
+void radioSend(String outputMessage) {
+  int mesLength = outputMessage.length();
+
+   if (debugMode) {
+    Serial.print("Sending to nRF24: '");
+    Serial.print(outputMessage);
+    Serial.print("', length of ");
+    Serial.println(mesLength);
+  }
+  
+  if (recieverRole == true) {
+    // Reciever
+    // Uses the pointer to the text as a character array
+    radio.writeAckPayload(pipeNumber, outputMessage.c_str(), mesLength);
+  }
+  else {
+    // Sender, request an acknoledgement
+    radio.write((outputMessage.c_str()), mesLength);
+  }
+
+}
