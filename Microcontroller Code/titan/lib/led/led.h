1db49a3acf47c07587c09325f84408b4b2c7d890
Savo <56771837+savob@users.noreply.github.com>
2022-09-06 23:05:12 -0400

    Added ledSet and comments

--- Microcontroller Code/titan/lib/led/led.h
+++ Microcontroller Code/titan/lib/led/led.h
@@ -4,9 +4,18 @@
 #include <Arduino.h>
 
 void setupLEDs();
-void ledOn(byte index);
-void ledOff(byte index);
-void invertLED(byte index);
+void ledOn(byte index); // Turn on a given status LED
+void ledOff(byte index); // Turn off a given status LED
+void ledSet(byte index, bool state); // Set a given status LED
+void ledInvert(byte index); // Invert the state of a given status LED
+
+/** @name blinkLEDBlocking
+   *  @brief Blink an LED a given number of times, blocking other code until done
+   *  @param index Status LED number to blink
+   *  @param period The period (in ms) of the blinking
+   *  @param times The number of blinks to exectute
+   *  @note Will block code (prevent execution until complete)
+   */
 void blinkLEDBlocking(byte index, int period, int times);
 
 /** @name heartBeat
